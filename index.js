be8bb54315724f6d4141fa15c9038079ad358880
Mir Nawaz <inbox.mirnawaz@gmail.com>
2019-01-25 12:53:37 +0500

    nodejs docker container :boom:

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+});
